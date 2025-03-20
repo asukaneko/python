b6444677508f8ec3fe5919fc87c42bd624f382b3
ycssbc <ycssbc@126.com>
2025-03-20 20:55:20 +0800

    2

--- 批量重命名/3.py
+++ 批量重命名/3.py
@@ -1,5 +1,7 @@
 #删除文件中多余的内容
 import os
+from bs4 import BeautifulSoup
+from html import unescape
 
 for filename in os.listdir('.'):
     if not filename.endswith('.txt'):
@@ -7,8 +9,10 @@ for filename in os.listdir('.'):
     with open(filename, 'r', encoding='utf-8', errors='ignore') as f:
         content = f.read()
         content = str(content)
-        content = content[170:-98]
-        content.replace("gt","")
+        #content = content[170:-98]
+        soup = BeautifulSoup(unescape(content), 'lxml')
+        content = soup.text
+        content.replace('>','')
     with open(filename, 'w', encoding='utf-8', errors='ignore') as f:
         f.write(content)
     print(filename+"已处理")
